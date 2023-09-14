e2c43566ed5afd25db620f4d4f0269017703564f
User <mcs9004158@gmail.com>
2023-09-14 23:18:09 +0300

    Разработка сервера

--- test.js
+++ test.js
@@ -18,25 +18,31 @@ app.get('/', function (req, res) {
   connection.query("select * from product;",
     function (err, results) {
       if (err) console.log(err);
-      else {console.log(req.params);res.render(__dirname + '/index.hbs', {Results: results,Id:req.params.id}); }
+      else { res.render(__dirname + '/index.hbs', { Results: results, Id: req.params.id }); }
     });
 });
 app.get("/:nameId/buy", function (req, res) {
   connection.query("select Name,Count from product where Name='" + req.params['nameId'] + "';",
     function (err, results) {
       if (err) console.log(err);
-      else { res.render(__dirname + '/buy.hbs', { Results: results}); }
+      else { res.render(__dirname + '/buy.hbs', { Results: results }); }
     });
 });
 app.get("/*.js", function (req, res) {
   res.sendFile(__dirname + req.url);
 });
 app.post("*", function (req, res) {
-  connection.query("UPDATE product set Count = Count -" + req.body.count + " where Count-" + req.body.count + ">0 and Name='" + req.body.name + "';", function (request, response) {
-    if (response['changedRows'] == 1) {console.log(response);
-      connection.query("insert buyers(First_name,Last_Name,House,Email,Name_Product,Count) values ('" + req.body.first_name + "','" + req.body.last_name + "','" + req.body.house + "','" + req.body.email + "','" + req.body.name + "'," + req.body.count + ");");
-      connection.query("select * from buyers where First_name='"+req.body.first_name+"' and Last_Name='"+req.body.last_name+"'",function (err, results){});
-    }
+  connection.query("insert ignore into buyers(First_name,Last_Name,House,Email,Name_Product,Count) values ('" + req.body.first_name + "','" + req.body.last_name + "','" + req.body.house + "','" + req.body.email + "','" + req.body.name + "'," + req.body.count + ");", function (err, results) {
+    if(results['affectedRows']==1){
+    connection.query("UPDATE product set Count = Count -" + req.body.count + " where Count-" + req.body.count + ">0 and Name='" + req.body.name + "';", function (request, response) {
+      if (response['changedRows'] == 1) {
+        connection.query("select * from buyers where First_name='" + req.body.first_name + "' and Last_Name='" + req.body.last_name + "'");
+        res.redirect("/");
+      }
+      else {
+        res.redirect("/Pen");
+      }
+    });}
     else{
       res.redirect("/Pen");
     }
