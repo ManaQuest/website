22fb38678e1644ab04eca6f513c50c887627d841
studentdep9 <studentdep9@ipps.by>
2023-09-15 15:54:06 +0300

    Создание сообщения о принятии заказа

--- test.js
+++ test.js
@@ -18,7 +18,12 @@ app.get('/', function (req, res) {
   connection.query("select * from product;",
     function (err, results) {
       if (err) console.log(err);
-      else { res.render(__dirname + '/index.hbs', { Results: results, Id: req.params.id }); }
+      else {
+        if(!req.query.res)
+          res.render(__dirname + '/index.hbs', { Results: results ,display:'none'});
+        else
+        res.render(__dirname + '/index.hbs', { Results: results ,display:'block',Id:req.query.res});
+      }
     });
 });
 app.get("/:nameId/buy", function (req, res) {
@@ -33,18 +38,19 @@ app.get("/*.js", function (req, res) {
 });
 app.post("*", function (req, res) {
   connection.query("insert ignore into buyers(First_name,Last_Name,House,Email,Name_Product,Count) values ('" + req.body.first_name + "','" + req.body.last_name + "','" + req.body.house + "','" + req.body.email + "','" + req.body.name + "'," + req.body.count + ");", function (err, results) {
-    if(results['affectedRows']==1){
-    connection.query("UPDATE product set Count = Count -" + req.body.count + " where Count-" + req.body.count + ">0 and Name='" + req.body.name + "';", function (request, response) {
-      if (response['changedRows'] == 1) {
-        connection.query("select * from buyers where First_name='" + req.body.first_name + "' and Last_Name='" + req.body.last_name + "'");
-        res.redirect("/");
-      }
-      else {
-        res.redirect("/Pen");
-      }
-    });}
-    else{
-      res.redirect("/Pen");
+    if (results['affectedRows'] == 1) {
+      connection.query("UPDATE product set Count = Count -" + req.body.count + " where Count-" + req.body.count + ">0 and Name='" + req.body.name + "';", function (request, response) {
+        if (response['changedRows'] == 1) {
+          connection.query("select * from buyers where First_name='" + req.body.first_name + "' and Last_Name='" + req.body.last_name + "'", function (er, result) { res.render(__dirname + '/letter.hbs', {Id: "Ваш номер заказа "+result[0]['Id']+". Вам выслано электронное письмо на почту.",display:'block' }); });
+        }
+        else {
+          connection.query("delete from buyers where First_name='" + req.body.first_name + "' and Last_Name='" + req.body.last_name + "'");
+          res.render(__dirname + '/index.hbs', { Results: results, Id: "Нужного количества товара не осталось.",display:'block'});
+        }
+      });
+    }
+    else {
+      res.redirect("/?res=Человек с таким именем, фамилией, email или домом уже существует. Измените заказ или обратитесь ко мне.");
     }
   });
 });
